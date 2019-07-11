d98fc4909243a776c0d8ba76c403700c49d66c69
Deep Patel <deep.ag123007@gmail.com>
2019-07-11 11:07:59 -0500

    done with family dictionary

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FamilyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, string>> myFamily = new Dictionary<string, Dictionary<string, string>>();
+
+            myFamily.Add("wife", new Dictionary<string, string>(){
+            {"name", "Janki"},
+            {"age", "26"}
+            });
+
+            myFamily.Add("father", new Dictionary<string, string>(){
+            {"name", "Ashvin"},
+            {"age", "50"}
+            });
+
+            myFamily.Add("mother", new Dictionary<string, string>(){
+            {"name", "Mamta"},
+            {"age", "48"}
+            });
+
+            myFamily.Add("sister", new Dictionary<string, string>(){
+            {"name", "Chandni"},
+            {"age", "26"}
+            });
+
+
+            myFamily.Add("brother", new Dictionary<string, string>(){
+            {"name", "Harsh"},
+            {"age", "30"}
+            });
+
+            myFamily.Add("friend", new Dictionary<string, string>(){
+            {"name", "Dan"},
+            {"age", "29"}
+            });
+
+
+
+            foreach (KeyValuePair<string, Dictionary<string, string>> familyMember in myFamily)
+            {
+                Console.WriteLine($"{familyMember.Value["name"]} is my {familyMember.Key} and is {familyMember.Value["age"]} years old.");
+            }
+        }
+    }
+}
